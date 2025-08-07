09b2525dbf659613112830323d26ed4dac4a158a
silverstar0-0 <3242645689@qq.com>
2025-08-08 00:34:21 +0800

    Create 链式前向星（带权无向图）.cpp

---
+++ 图论/链式前向星建图/链式前向星（带权无向图）.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn=1e5+5; 
+int n,m,point[maxn],nextx[2*maxn],to[2*maxn],value[2*maxn],cnt=1;
+void create(void){
+	int from,togo,val;
+	cin>>n>>m;
+	for(int i=1;i<=m;i++){
+		cin>>from>>togo>>val;
+		nextx[cnt]=point[from];
+		nextx[cnt+1]=point[togo];
+		point[from]=cnt;
+		point[togo]=cnt+1;
+		to[cnt]=togo;
+		to[cnt+1]=from;
+		value[cnt]=value[cnt+1]=val;
+		cnt+=2;
+	}
+}
+//n为节点数量，m为边数量
+//本程序适用于不带权无向图的建图
