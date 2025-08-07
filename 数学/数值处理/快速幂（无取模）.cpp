d97c3f66cdde66dc9daf63317132ab0cdec4c41a
silverstar0-0 <3242645689@qq.com>
2025-08-08 00:54:55 +0800

    Update 快速幂（无取模）.cpp

--- 数学/数值处理/快速幂（无取模）.cpp
+++ 数学/数值处理/快速幂（无取模）.cpp
@@ -1,12 +1,11 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void ksm(long long a,long long b){
+long long ksm(long long a,long long b){
 	long long out_put=1;//a为底，b为幂次，out_put为输出结果
-	cin>>a>>b;
 	while(b>0){
 		if(b&1==1) out_put=out_put*a;
 		a*=a;
 		b>>=1;
 	}
-	return 0;
+	return out_put;
 }
