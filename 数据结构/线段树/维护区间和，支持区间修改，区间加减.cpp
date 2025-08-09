2f886b49dc71aa9a3c786a7cc13b401c751f7bee
silverstar0-0 <3242645689@qq.com>
2025-08-10 05:25:28 +0800

    Update 维护区间和，支持区间修改，区间加减.cpp

--- 数据结构/线段树/维护区间和，支持区间修改，区间加减.cpp
+++ 数据结构/线段树/维护区间和，支持区间修改，区间加减.cpp
@@ -3,17 +3,17 @@ using namespace std;
 const int maxn=1e5+5;
 int tree[4*maxn],lazy[4*maxn],change[4*maxn];
 bool ischanged[4*maxn];
-vector<int> v;
+int a[maxn];
 void create(int l,int r,int pos){
 	if(l==r){
-		tree[pos]=v[l];
+		tree[pos]=a[l];
 		return;
 	}
 	else{
 		int mid=(l+r)>>1;
 		create(l,mid,pos*2);
 		create(mid+1,r,pos*2+1);
-		tree[pos]=tree[pos*2]+tree[pos*2+1];
+		tree[pos]=max(tree[pos*2],tree[pos*2+1]);
 		return;
 	}
 }
@@ -26,11 +26,25 @@ void push(int l,int r,int pos,int x){
 }
 void rechange(int l,int r,int pos,int x){
 	tree[pos]=(r-l+1)*x;
+	lazy[pos]=0;
 	if(l!=r){
 		change[pos]=x;
 		ischanged[pos]=true;
 	}
 	return;
+}
+void pushdown(int l, int r, int pos) {
+    int mid=(l+r)>>1;
+    if(ischanged[pos]){
+        rechange(l,mid,pos*2,change[pos]);
+        rechange(mid+1,r,pos*2+1,change[pos]);
+        ischanged[pos]=false;
+    }
+    if(lazy[pos]!=0){
+        push(l,mid,pos*2,lazy[pos]);
+        push(mid+1,r,pos*2+1,lazy[pos]);
+        lazy[pos]=0;
+    }
 }
 void add(int l,int r,int pos,int tarl,int tarr,int x){
 	if(l>=tarl&&r<=tarr){
@@ -39,16 +53,7 @@ void add(int l,int r,int pos,int tarl,int tarr,int x){
 		return;
 	}
 	int mid=(l+r)>>1;
-	if(ischanged[pos]==true){
-		rechange(l,mid,pos*2,change[pos]);
-		rechange(mid+1,r,pos*2+1,change[pos]);
-		ischanged[pos]=false;
-	}
-	if(lazy[pos]!=0){
-		push(l,mid,pos*2,lazy[pos]);
-		push(mid+1,r,pos*2+1,lazy[pos]);
-		lazy[pos]=0;
-	}
+	pushdown(l,r,pos);
 	if(tarl<=mid) add(l,mid,pos*2,tarl,tarr,x);
 	if(tarr>mid) add(mid+1,r,pos*2+1,tarl,tarr,x);
 	tree[pos]=tree[pos*2]+tree[pos*2+1];
@@ -65,6 +70,7 @@ void reset(int l,int r,int pos,int tarl,int tarr,int x){
 		return;
 	}
 	int mid=(l+r)>>1;
+	pushdown(l,r,pos);
 	if(tarl<=mid) reset(l,mid,pos*2,tarl,tarr,x);
 	if(tarr>mid) reset(mid+1,r,pos*2+1,tarl,tarr,x);
 	tree[pos]=tree[pos*2]+tree[pos*2+1];
@@ -73,25 +79,18 @@ void reset(int l,int r,int pos,int tarl,int tarr,int x){
 int query(int l,int r,int pos,int tarl,int tarr){
 	if(l>=tarl&&r<=tarr) return tree[pos];
 	int mid=(l+r)>>1,ans=0;
-	if(ischanged[pos]==true){
-		rechange(l,mid,pos*2,change[pos]);
-		rechange(mid+1,r,pos*2+1,change[pos]);
-	}
-	push(l,mid,pos*2,lazy[pos]);
-	push(mid+1,r,pos*2,lazy[pos]);
+	pushdown(l,r,pos);
 	if(tarl<=mid) ans+=query(l,mid,pos*2,tarl,tarr);
 	if(tarr>mid) ans+=query(mid+1,r,pos*2+1,tarl,tarr);
-	tree[pos]=tree[pos*2]+tree[pos*2+1];
 	return ans;
 }
 int main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
     int n,m,temp,l,r,x;
     cin>>n>>m; 
-    v.push_back(0);
-    for(int i=0;i<n;i++){
-		cin>>temp;
-		v.push_back(temp);
-	}
+    for(int i=1;i<=n;i++) cin>>a[i];
 	create(1,n,1);
 	return 0;
 }
